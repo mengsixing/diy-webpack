3c0fdad4bc741930228250d315101ba2e048894e
尹珩力 <yinhengli@sina.com>
2018-10-20 17:54:57 +0800

    undefined

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # diy-webpack
+
+手动写一个 webpack 解析器。
+
+## craeteAsset
+
+1、使用 node file 模块获取文件内容。
+
+2、使用 babylon 将文件内容转换成 ast 抽象语法树。
+
+3、使用 babel-traverse 对 ast 进行遍历，将入口文件的依赖保存起来。
+
+4、使用 babel.transformFromAst 将 ast 转换成可执行的 js 代码。
+
+5、返回经过处理后的一个模块，包含：模块 id，模块 filename，依赖项 dependencies，code 等信息。
+
+## createGraph
+
+1、传入入口文件路径，处理入口模块
+
+2、新建一个数组，先加入入口模块，然后循环把入口模块所依赖的模块都加入到数组中。
+
+3、生成包含所有模块的数组，并返回。
+
+## bundle
+
+1、传入生成的模块数组。
+
+2、遍历数组，把执行的 code 加入到一个函数级作用域中，并增加一个子依赖的属性 mapping。
+
+3、创建一个 require 方法，因为打包出来的代码是 commonjs 语法，这里为了解析 require 方法。
+
+4、require 中循环加载所有依赖项，并执行。
+
+5、返回处理结果。
--- diy-webpack.js
+++ diy-webpack.js
@@ -2,29 +2,36 @@ const fs = require("fs");
 const path = require("path");
 const babylon = require("babylon");
 const traverse = require("babel-traverse").default;
-const babel = require('babel-core');
+const babel = require("babel-core");
 
 let ID = 0;
 
+// 读取内容并提取它的依赖关系
 function craeteAsset(filename) {
+  // 以字符串的形式读取文件
   const content = fs.readFileSync(filename, "utf-8");
 
+  // 转换字符串为ast抽象语法树
   const ast = babylon.parse(content, {
     sourceType: "module"
   });
 
   const dependencies = [];
 
+  // 遍历抽象语法树
   traverse(ast, {
+    // 每当遍历到import语法的时候
     ImportDeclaration: ({ node }) => {
+      // 把依赖的模块加入到数组中
       dependencies.push(node.source.value);
     }
   });
 
   const id = ID++;
 
-  const {code} = babel.transformFromAst(ast,null,{
-    presets:['env']
+  // 转换为浏览器可运行的代码
+  const { code } = babel.transformFromAst(ast, null, {
+    presets: ["env"]
   });
 
   return {
@@ -35,13 +42,17 @@ function craeteAsset(filename) {
   };
 }
 
+// 从入口开始，分析所有依赖项，形成依赖图，采用深度优先遍历
 function createGraph(entry) {
   const mainAsset = craeteAsset(entry);
+
+  // 定义一个保存依赖项的数组
   const queue = [mainAsset];
-  debugger
+
   for (const asset of queue) {
     const dirname = path.dirname(asset.filename);
 
+    // 定义一个保存子依赖项的属性
     asset.mapping = {};
 
     asset.dependencies.forEach(relativePath => {
@@ -49,16 +60,19 @@ function createGraph(entry) {
 
       const child = craeteAsset(absolutePath);
 
+      // 给子依赖项赋值
       asset.mapping[relativePath] = child.id;
 
+      // 将子依赖也加入队列中，循环处理
       queue.push(child);
     });
   }
   return queue;
 }
 
+// 根据生成的依赖关系图，生成浏览器可执行文件
 function bundle(graph) {
-  let modules = '';
+  let modules = "";
 
   graph.forEach(mod => {
     modules += `${mod.id}:[
@@ -66,20 +80,23 @@ function bundle(graph) {
         ${mod.code}
       },
       ${JSON.stringify(mod.mapping)},
-    ],`
+    ],`;
   });
 
   const result = `
     (function(modules){
+      // 创建一个require()函数: 它接受一个 模块ID 并在我们之前构建的模块对象查找它.
       function require(id){
-        const [fn,mapping] = modules[id];
+        const [fn, mapping] = modules[id];
 
         function localRequire(relativePath){
+          // 根据mapping的路径，找到对应的模块id
           return require(mapping[relativePath]);
         }
 
         const module = {exports:{}};
 
+        // 执行转换后的代码，并输出内容。
         fn(localRequire,module,module.exports);
 
         return module.exports;
